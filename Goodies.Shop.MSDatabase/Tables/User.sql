262ece8e7900c3aefb7ff87aae174868b442330d
Goldin Baloyi <GoldinB@hollywoodbets.net>
2024-03-25 14:37:43 +0200

    #14 Added tables needed for storing users

---
+++ Goodies.Shop.MSDatabase/Tables/User.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE [dbo].[User](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](500) NULL,
+	[LastName] [varchar](100) NULL,
+	[EmailAddress] [varchar](100) NULL,
+	[Password] [varchar](100) NULL,
+	[Cellphone] [varchar](50) NULL,
+	[UserTypeID] [int] NULL,
+	[UserAddressID] [int] NULL,
+	[DateCreated] [datetime] NULL,
+	[DateModified] [datetime] NULL,
+	[DateLasteOrdered] [datetime] NULL,
+	[IsApproved] [bit] NULL,
+	[IsAdmin] [bit] NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [UK_User_Email] UNIQUE NONCLUSTERED 
+(
+	[EmailAddress] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_UserAddress] FOREIGN KEY([UserAddressID])
+REFERENCES [dbo].[UserAddress] ([UserAddressID])
+GO
+
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_UserAddress]
+GO
+
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_UserType] FOREIGN KEY([UserTypeID])
+REFERENCES [dbo].[UserType] ([UserTypeID])
+GO
+
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_UserType]
+GO
---
+++ Goodies.Shop.MSDatabase/Tables/UserAddress.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[UserAddress](
+	[UserAddressID] [int] IDENTITY(1,1) NOT NULL,
+	[PhysicalAddress1] [varchar](150) NULL,
+	[PhysicalAddress2] [varchar](150) NULL,
+	[PhysicalAddress3] [varchar](150) NULL,
+	[PhysicalAddress4] [varchar](150) NULL,
+	[PhysicalAddressCode] [varchar](50) NULL,
+	[PostalAddress1] [varchar](150) NULL,
+	[PostalAddress2] [varchar](150) NULL,
+	[PostalAddress3] [varchar](150) NULL,
+	[PostalAddress4] [varchar](150) NULL,
+	[PostalAddressCode] [varchar](50) NULL,
+	[City] [varchar](50) NULL,
+	[Pronvince] [varchar](50) NULL,
+	[Country] [varchar](50) NULL,
+	[Telephone] [varchar](50) NULL,
+	[DateCreated] [datetime] NULL,
+ CONSTRAINT [PK_UserAddress] PRIMARY KEY CLUSTERED 
+(
+	[UserAddressID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
